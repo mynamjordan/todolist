5b5259997654259d13783fdbb47044b68b38ca20
mynamjordan <103250087+mynamjordan@users.noreply.github.com>
2022-04-08 18:28:23 +0800

    save

--- js/script.js
+++ js/script.js
@@ -3,14 +3,18 @@ let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
 let todoListContainer = document.getElementById("todo-list")
 
+let templateElement = document.getElementById("list-item-template")
+let template = templateElement.innerHTML
 
 
 // Step 2: Write funtion to implement the behavior
 function onAddTaskClicked(event){
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
-    console.log(taskName)
-    todoListContainer.insertAdjacentHTML("afterbegin", taskName);
+    
+
+    let taskHTML = template.replace("!-- TASK_NAME -->", taskName);
+    todoListContainer.insertAdjacentHTML("afterbegin", taskHTML);
 }
 
 
